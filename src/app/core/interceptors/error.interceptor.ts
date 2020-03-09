cc3f655c00374b311bf295eed7edd1ac5a7ce03c
sangress <shai.angress@gmail.com>
2020-03-09 18:10:06 +0200

    fix: error handling - parallel login #SW-854

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -8,69 +8,44 @@ import { AuthService } from '../../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private dialog: MatDialog, private authService: AuthService) {}
+	constructor(private dialog: MatDialog, private authService: AuthService) { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    return next.handle(req)
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          let modalTitle;
-          let modalMessage;
+	intercept(req: HttpRequest<any>, next: HttpHandler) {
+		return next.handle(req)
+			.pipe(
+				catchError((error: HttpErrorResponse) => {
+					let modalTitle = 'General Error';
+					let modalMessage = 'An unknown error occurred!';
 
-          if(false) {
-          // if(error.status === 401) {
-            // modalTitle = 'Session Idle';
-            // modalMessage = 'You are logged out because of session idle';
-            // this.dialog.open(ErrorModalComponent, {
-            //   width: '500px',
-            //   height: '200px',
-            //   data: { 
-            //     title: modalTitle,
-            //     message: modalMessage
-            //   }
-            // });
-            // this.authService.logout();
-          } else {
-            modalMessage = 'An unknown error occurred!';
+					if (error.status) {
+						modalTitle = `Error ${error.status}`;
+						if (error.status === 401) {
+							modalTitle = 'Session Idle';
+							modalMessage = 'You are logged out because of session idle.';
+							this.authService.logout();
+						} else if(error.status === 412) {
+							if (error.error) {
+								if (error.error.errorMessageId === 'pmErrorLoginBadLoginDetails') {
+									modalMessage = 'Bad login details';
+								} else if (error.error.errorMessageId === 'pmErrorLoginPasswordExpiry') {
+									modalMessage = 'Your password has been expired';
+								}
+							}
+							this.authService.logout();
+						}
+					}
 
-            if(error.status === 500) {
-              modalTitle = 'Error 500';
-            } else if (error.status === 400) {
-              modalTitle = 'Error 400';
-              return throwError(error);
-            } else if (error.status === 403) {
-              modalTitle = 'Error 403';
-            } else if (error.status === 404) {
-              modalTitle = 'Error 404';
-              return throwError(error);
-            } else if (error.status === 401) {
-              modalTitle = 'Error 401';
-              return throwError(error);
-            } else if (error.status === 412) {
-              modalTitle = 'Error 412';
-              if (error.error.errorMessageId === 'pmErrorLoginBadLoginDetails') {
-                modalMessage = 'Bad login details';
-              } else if (error.error.errorMessageId === 'pmErrorLoginPasswordExpiry') { // TODO need to know the ids
-                modalMessage = 'Your password has been expired'; // TODO need to know texts
-              }
-            } else {
-              modalTitle = 'General Error';
-            }
+					this.dialog.open(ErrorModalComponent, {
+						width: '500px',
+						height: '200px',
+						data: {
+							title: modalTitle,
+							message: modalMessage
+						}
+					});
 
-            if (error.error.errorMessageId !== 'pmErrorLoginBadLoginDetails') {
-              this.dialog.open(ErrorModalComponent, {
-                width: '500px',
-                height: '200px',
-                data: { 
-                  title: modalTitle,
-                  message: modalMessage
-                }
-              });
-            }
-          }
-
-          return throwError(error);
-        })
-      );
-  }
+					return throwError(error);
+				})
+			);
+	}
 }
