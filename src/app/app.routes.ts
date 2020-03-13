344ab5049021609b535abfaec1eab9026de64a2e
yonikan <yonikanwork@gmail.com>
2020-03-13 18:47:44 +0200

    added new route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { AuthComponent } from './auth/auth/auth.component';
 import { MenuComponent } from './core/components/menu/menu.component';
+import { ValidatedEventsToastComponent } from './core/components/validated-events-toast/validated-events-toast.component';
 
 const routes: Routes = [
   { path: 'team-overview', loadChildren: () => import('./team-overview/team-overview.module')
     .then(m => m.TeamOverviewModule), canLoad: [AuthGuard]},
   // { path: 'team-event-validation', loadChildren: () => import('./team-event-validation/team-event-validation.module')
   //   .then(m => m.TeamEventValidationModule), canLoad: [AuthGuard]},
+  { path: 'toast', component: ValidatedEventsToastComponent },
   { path: 'login', component: AuthComponent },
   { path: 'menu', component: MenuComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/team-overview', pathMatch: 'full' },
